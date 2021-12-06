996ad5baeae5d66e1ccc4f3a1fdb4daca1a3af65
Condence <55905870+Condence@users.noreply.github.com>
2021-12-06 14:52:09 -0700

    cors

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const app = require("express")();
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Origin", "*"); 
   next();
 });
 const http = require("http").createServer(app);
