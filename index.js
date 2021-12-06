40a65a392eb87831c8d95032dda1ae40b21c6193
Condence <55905870+Condence@users.noreply.github.com>
2021-12-06 16:54:31 -0700

    cors

--- index.js
+++ index.js
@@ -7,11 +7,16 @@ const cors = require('cors');
 const path = require('path');
 const io = require("socket.io")(https, {
   cors: {
-    origin: "*",  
-    methods: ["GET", "POST"]
+    origins: [
+      "https://canjeatesting.erus.mx",
+      "http://localhost:4200",
+      "http://localhost:8080",
+      "https://erp.erus.mx",
+      "https://erpupdate.erus.mx",
+      "https://localhost:4200",
+    ]
   },
 });
-app.use(cors());
 var port = process.env.PORT || 8005;
 
  
