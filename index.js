9ab608d880c6388fe5b3c25c65d623a909ed6ec7
Condence <55905870+Condence@users.noreply.github.com>
2021-12-06 17:07:33 -0700

    http

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var https = require('https');
 const fs = require("fs");
 const cors = require('cors');
 const path = require('path');
-const io = require("socket.io")(https, {
+const io = require("socket.io")(http, {
   path: "/socket.io",
   cors: {
     origin: "*",
