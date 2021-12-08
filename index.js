fc1c851790ae2c5056f4831e7ec79af9fce578bf
Condence <55905870+Condence@users.noreply.github.com>
2021-12-07 17:09:50 -0700

    commit

--- index.js
+++ index.js
@@ -1,74 +1,60 @@
-const app = require("express")();
- 
-const http = require("http");
+var fs = require('fs');
 var https = require('https');
-const fs = require("fs");
-const cors = require('cors');
-const path = require('path');
-const io = require("socket.io")(http, {
-  path: "/socket.io",
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"], 
-    credentials: true
-  }
-});
-var port = process.env.PORT || 8005;
+const http = require("http");
+var express = require('express');
+var app = express();
 
- 
 const getMostRecentFile = (dir) => {
-  const files = orderReccentFiles(dir);
-  return files.length ? files[0] : undefined;
+    const files = orderReccentFiles(dir);
+    return files.length ? files[0] : undefined;
 };
-
 const orderReccentFiles = (dir) => {
-  return fs.readdirSync(dir)
+return fs.readdirSync(dir)
     .filter((file) => fs.lstatSync(path.join(dir, file)).isFile())
     .filter(file => path.extname(file).toLocaleLowerCase() !== '.cache')
     .map((file) => ({ file, mtime: fs.lstatSync(path.join(dir, file)).mtime }))
     .sort((a, b) => b.mtime.getTime() - a.mtime.getTime());
 };
-app.use(cors());
-io.use((socket, next) => {
-  const token = socket.handshake.auth.token;
-  if (token === "dedb2b7e-c7c0-4ed9-b7dd-197908574aae") next();
-  else {
-    console.log("bad user");
-    return "badRequest";
-  }
-});
+
+var server;
+var serverPort;
+var options;
+if(Boolean(process.env.PROD)===true){
+  const key = getMostRecentFile('/home/erusboxadmin/ssl/keys/');
+  const cert = getMostRecentFile('/home/erusboxadmin/ssl/certs/');
+  
+  options = {
+    key: fs.readFileSync(`/home/erusboxadmin/ssl/keys/${key.file}`),
+    cert: fs.readFileSync(`/home/erusboxadmin/ssl/certs/${cert.file}`)
+  };
+   
+  
+   server = https.createServer(options, app);
+} else {
+  server = http.createServer(options, app);
+}
+serverPort = 8005;
+var io = require('socket.io')(server);
 
 app.get("/", (req, res) => {
-  res.send("<h1>ERUS</h1>");
+    res.send("<h1>ERUS</h1>");
 });
 
 io.on("connection", (socket) => {
-  // console.log("a user connected");
-  socket.on("disconnect", () => {
-    // console.log("user disconnected");
-  });
+    // console.log("a user connected");
+    socket.on("disconnect", () => {
+        // console.log("user disconnected");
+    });
 
-  socket.on("message", (msg) => {
-    // console.log("message: " + msg);
-    var toJson = JSON.parse(msg);
+    socket.on("message", (msg) => {
+        // console.log("message: " + msg);
+        var toJson = JSON.parse(msg);
 
-    io.emit(toJson.channel, toJson.message);
-    // console.log(msg);
-  });
+        io.emit(toJson.channel, toJson.message);
+        // console.log(msg);
+    });
 });
 
-if(Boolean(process.env.PROD)===true){
-  const key = getMostRecentFile('/home/erusboxadmin/ssl/keys/');
-  const cert = getMostRecentFile('/home/erusboxadmin/ssl/certs/')
-  return https.createServer({
-    key: fs.readFileSync(`/home/erusboxadmin/ssl/keys/${key.file}`),
-    cert: fs.readFileSync(`/home/erusboxadmin/ssl/certs/${cert.file}`)
-  },app).listen(process.env.PORT||8005, () => {
-    console.log(`Servicio de créditos corriendo en el puerto ${process.env.PORT||8005}!`)
-  });
-}else{
-  return app.listen(process.env.PORT||8005, () => {
-    console.log(`Servicio de créditos corriendo en el puerto ${process.env.PORT||8005}!`)
-  });
-}
- 
+server.listen(serverPort, function() {
+  console.log('server up and running at %s port', serverPort);
+});
